b51a1961f8a0fd08f10cbc1b0e67293f031532ea
math4matix <kashliak.vlad@gmail.com>
2024-05-17 19:54:41 +0300

    Main menu ver alpha

---
+++ Assets/Scripts/GameStartMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStartMenu : MonoBehaviour
+{
+    [Header("UI Pages")]
+    public GameObject mainMenu;
+
+    [Header("Main Menu Buttons")]
+    public Button startButton;
+    public Button quitButton;
+
+    public List<Button> returnButtons;
+
+    void Start()
+    {
+        EnableMainMenu();
+
+        //Hook events
+        startButton.onClick.AddListener(StartGame);
+        quitButton.onClick.AddListener(QuitGame);
+
+        foreach (var item in returnButtons)
+        {
+            item.onClick.AddListener(EnableMainMenu);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        HideAll();
+        SceneTransitionManager.singleton.GoToSceneAsync(1);
+    }
+
+    public void HideAll()
+    {
+        mainMenu.SetActive(false);
+    }
+
+    public void EnableMainMenu()
+    {
+        
+    }
+    public void EnableOption()
+    {
+        mainMenu.SetActive(false);
+    }
+    public void EnableAbout()
+    {
+        mainMenu.SetActive(false);
+    }
+}
