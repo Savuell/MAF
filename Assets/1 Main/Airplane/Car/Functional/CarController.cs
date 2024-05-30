510793771347f1a8815da28819eaccf61ffd7eb2
Savuell <savuelliy@gmail.com>
2024-05-30 10:19:01 +0300

    New Model

--- Assets/1 Main/Airplane/Car/Functional/CarController.cs
+++ Assets/1 Main/Airplane/Car/Functional/CarController.cs
@@ -6,17 +6,8 @@ public class CarController : MonoBehaviour
 {
     [SerializeField] private SteeringWheel steeringWheel;
     [SerializeField] private WheelsAngle wheels;
-    [SerializeField] private GameObject tech, techMain, underHood;
-    [SerializeField] private CarHinge hood;
     void Update()
     {
         wheels.rotate = steeringWheel.steerAngle/20;
-        ShowTech(hood.opened||hood.isOpen);
-    }
-    void ShowTech(bool hide)
-    {
-        tech.SetActive(hide);
-        techMain.SetActive(hide);
-        underHood.SetActive(hide);
     }
 }
